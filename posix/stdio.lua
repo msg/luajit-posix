5df046d041d7c45f02639ca3a47274af018c9b70
Matt Gerassimoff <mgeras@gmail.com>
2021-06-15 10:18:52 -0700

    add perror() to stdio.

--- posix/stdio.lua
+++ posix/stdio.lua
@@ -11,6 +11,7 @@ ffi.cdef([[
 int printf(const char *format, ...);
 int sprintf(char *s, const char *format, ...);
 int snprintf(char *s, size_t maxlen, const char *format, ...);
+int perror(char *s);
 ]])
 
 return stdio
