327b8783f1f7c6248b6069f285a62fe95a4ec007
Matt Gerassimoff <mgeras@gmail.com>
2021-06-29 20:24:52 -0700

    add FILE * functions.

--- posix/stdio.lua
+++ posix/stdio.lua
@@ -8,6 +8,17 @@ local ffi	= require('ffi')
 		  require('posix.sys.types')
 
 ffi.cdef([[
+typedef struct _IO_FILE FILE;
+
+FILE *fopen(const char *__restrict, const char *__restrict);
+int fclose(FILE *);
+int ferror(FILE *stream);
+int fflush(FILE *stream);
+
+int fprintf(FILE *__restrict, const char *__restrict, ...);
+size_t fread(void *__restrict, size_t, size_t, FILE *__restrict);
+size_t fwrite(const void *__restrict, size_t, size_t, FILE *__restrict);
+
 int printf(const char *format, ...);
 int sprintf(char *s, const char *format, ...);
 int snprintf(char *s, size_t maxlen, const char *format, ...);
