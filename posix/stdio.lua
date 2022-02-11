43f3e03ecdca7e21d6d9db6626647f0f098a266e
Matt Gerassimoff <mgeras@gmail.com>
2022-02-11 10:35:55 -0700

    add rename*() functions.

--- posix/stdio.lua
+++ posix/stdio.lua
@@ -23,6 +23,17 @@ int printf(const char *format, ...);
 int sprintf(char *s, const char *format, ...);
 int snprintf(char *s, size_t maxlen, const char *format, ...);
 int perror(char *s);
+
+int rename(const char *old, const char *new);
+int renameat(int oldfd, const char *old, int newfd, const char *new);
+int renameat2(int oldfd, const char *old, int newfd, const char *new, int flags);
+
+enum {
+	RENAME_NOREPLACE	= 1 << 0,
+	RENAME_EXCHANGE		= 1 << 1,
+	RENAME_WHITEOUT		= 1 << 2,
+};
+
 ]])
 
 return stdio
