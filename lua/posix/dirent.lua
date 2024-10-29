0c08b7b883765cd78f76fae606268ca8ed15571d
Matt Gerassimoff <mgeras@gmail.com>
2024-10-29 06:59:44 -0500

    stupid syntax error.

--- lua/posix/dirent.lua
+++ lua/posix/dirent.lua
@@ -57,7 +57,7 @@ int readdir_r(DIR *, struct dirent *, struct dirent **);
 void rewinddir(DIR *);
 int scandir(const char *, struct dirent ***,
 		int (*)(const struct dirent *),
-		int (*)(const struct dirent **, const struct dirent **);
+		int (*)(const struct dirent **, const struct dirent **));
 void seekdir(DIR *, long);
 long telldir(DIR *);
 ]])
